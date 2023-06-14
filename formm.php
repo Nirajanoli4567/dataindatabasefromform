de86146963cf35dd379c1e09a34b3b5aea4ca7f3
Nirajan oli <110576461+Nirajanoli4567@users.noreply.github.com>
2023-06-14 15:18:13 +0545

    Add files via upload

---
+++ formm.php
@@ -0,0 +1,39 @@
+<?php
+if(isset($_POST['submit']))
+{
+    $name=$_POST['name'];
+    $pass=$_POST['password'];
+    $email=$_POST['email'];
+
+
+    // echo $name."<br>";
+    // echo $pass."<br>";
+    // echo $email;
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="d1";
+
+    $conn=mysqli_connect($servername,$username,$password,$dbname);
+
+    if(!$conn)
+    {
+        die ("cannot connect the database").mysqli_connect_error();
+    }
+    else
+    {
+        echo " connected successfully" ."<br>";
+    }
+
+    $sql="INSERT INTO NIRAJAN VALUES ('$name','$pass','$email')";
+    if(mysqli_query($conn,$sql))
+    {
+        echo " data inserted";
+    }
+    else{
+        echo " daatase are not inserted in the database";
+    }
+}
+
+?>
